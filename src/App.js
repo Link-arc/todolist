de3070810b226d1e6dc749a6696a19fbfb6e75cd
dissolver <indra.elvian@gmail.com>
2019-08-30 05:16:29 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
+
+class TodoList extends React.Component {
+  render() {
+    const { items, onClick } = this.props;
+    return (<ul onClick={onClick}>
+      {items.map((item, index) =>
+          <TodoItem item={item} key={index} onClick={this.handleItemClick.bind(this, item)}/>)}
+    </ul>);
+  }
+
+  handleItemClick(item, event) {
+    // Write your code here
+    const { onItemClick } = this.props
+    if (!item.done) {
+      onItemClick(item, event)
+    } else
+      event.stopPropagation()
+  }
 }
 
+
+const items = [ { text: 'Buy grocery', done: true },
+  { text: 'Play guitar', done: false },
+  { text: 'Romantic dinner', done: false }
+];
+
+const App = (props) => <TodoList
+    items={props.items}
+    onItemClick={(item, event) => { console.log(item, event) }}
+/>;
+
 export default App;
+
+
+ReactDOM.render(<App items={items} />, document.getElementById('root'));
